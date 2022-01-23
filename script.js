f2e68cef0dbce8b293b840dbcdbabde51a6ee730
ILikeCode1 <82778420+ILikeCode1@users.noreply.github.com>
2022-01-23 13:26:44 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let mobileMenu = document.getElementById("mobileMenu");
+let mobileNav = document.getElementById("mobileNav");
+
+mobileNav.style.display = "none";
+mobileMenu.style.opacity = 1;
+
+mobileMenu.onclick = function mobileMenuT() {
+  if (mobileNav.style.display == "none") {
+    mobileNav.style.display = "block";
+    mobileMenu.style.opacity = 0.5;
+  } else {
+    mobileNav.style.display = "none";
+    mobileMenu.style.opacity = 1;
+  }
+};
