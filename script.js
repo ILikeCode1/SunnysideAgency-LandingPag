913005a45628310c8afe2a56afcb4ae9cf396fc7
ILikeCode1 <82778420+ILikeCode1@users.noreply.github.com>
2022-02-27 21:21:29 -0800

    Update script.js
    fixed mobile menu appearing in desktop site

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let mobileMenu = document.getElementById("mobileMenu");
 let mobileNav = document.getElementById("mobileNav");
 
-mobileNav.style.display = "none";
-mobileMenu.style.opacity = 1;
+// Activates mobile menu when clicked
+mobileMenu.addEventListener("click", mobileMenuT);
 
-mobileMenu.onclick = function mobileMenuT() {
+function mobileMenuT() {
   if (mobileNav.style.display == "none") {
     mobileNav.style.display = "block";
     mobileMenu.style.opacity = 0.7;
@@ -12,4 +12,14 @@ mobileMenu.onclick = function mobileMenuT() {
     mobileNav.style.display = "none";
     mobileMenu.style.opacity = 1;
   }
-};
+}
+
+// Prevents mobile menu from appearing in desktop site
+var e = window.matchMedia("(min-width: 750px)");
+e.addListener(mediaQuery);
+
+function mediaQuery(e) {
+  if (e.matches) {
+    mobileNav.style.display = "none";
+  }
+}
